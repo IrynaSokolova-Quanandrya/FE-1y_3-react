c4dc9ec3df46fb1395d2496de6acbfe554b8dce6
Iryna <irasokolova25@ukr.net>
2024-01-27 00:02:30 +0200

    створила план

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,6 +6,16 @@ import { Alert } from '../Alert/Alert';
 import { Container, Title } from './App.styled';
 import { GlobalStyle } from '../../GlobalStyle.styled';
 
+/**
+ * робимо todo список
+ * декілька форм та однаковий id
+ * створюємо нову форму TodoEditor
+ * додаємо нові завдання в стейт не мутуючи масиву
+ * видаляємо завдання не мутуючи масиву * 
+ * radio
+ * чекбокси
+ */
+
 export function App() {
   return(
     <div>
@@ -15,11 +25,11 @@ export function App() {
       </Container> */}
       
 
-       <PaintingList painting={painting}/>
-      <ColorPicker options={colorPickerOptions}/>
-      <Alert text="Останнє попередження" type="warning"/>
+       {/* <PaintingList painting={painting}/> */}
+      {/* <ColorPicker options={colorPickerOptions}/> */}
+      {/* <Alert text="Останнє попередження" type="warning"/>
       <Alert text="Ура! Все ок!" type="success"/>
-      <Alert text="О жах - все пропало!" type="error"/>
+      <Alert text="О жах - все пропало!" type="error"/> */}
       <GlobalStyle/>
     </div>
   )
