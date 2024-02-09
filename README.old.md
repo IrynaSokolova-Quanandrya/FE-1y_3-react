e4407f8df896baa3a3bc1d3493feaabc5c9ea819
Iryna <irasokolova25@ukr.net>
2024-02-09 23:15:54 +0200

    add modal

--- README.old.md
+++ README.old.md
@@ -3,6 +3,6 @@
 3. зберігаємо тудушки в locale storage
 4. зчитуємо з locale storage
 5. Модалне вікно
-6.
-7.
+6. createPortal
+7. Закриваємо по Esc та по backdrop
 8.
---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import Modal from "../Modal/Modal";
+
+export class App extends Component{
+  render(){
+    return(
+      <Modal>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae libero praesentium officia assumenda tenetur modi ducimus dicta labore, fugiat facilis aliquid expedita dolores sed. Nihil nisi perspiciatis cupiditate earum nulla.</p>
+      </Modal>
+    )
+  }
+}
