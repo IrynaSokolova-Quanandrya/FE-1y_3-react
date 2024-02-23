e5f20053dd0d723b0f1ffee13093d676e876ec43
Iryna <irasokolova25@ukr.net>
2024-02-23 23:09:42 +0200

    prepare

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,41 +1,15 @@
-import painting from '../../painting.json';
-import {colorPickerOptions} from '../../colorOptions';
-import { PaintingList } from '../PaintingList/PaintingList';
-import { ColorPicker } from '../ColorPicker/ColorPicker';
-import { Alert } from '../Alert/Alert';
-import { Container, Title } from './App.styled';
-import { PokemonInfo } from '../OldPokemon/PokemonInfo';
-import { PokemonForm } from "../OldPokemon/PokemonForm";
 import { GlobalStyle } from '../../GlobalStyle.styled';
 import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 
 export class App extends Component {
-  state = {
-    pokemonName: '',
-  };
-
-  handleFormSubmit = pokemonName => {
-    this.setState({ pokemonName });
-  };
   render() {
     return(
     <div>
  <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-        <PokemonInfo pokemonName={this.state.pokemonName} />
+     
         <ToastContainer autoClose={3000} />
       </div>
-      {/* <Container>
-        <Title>Title</Title>
-      </Container> */}
-      
-
-       <PaintingList painting={painting}/>
-      <ColorPicker options={colorPickerOptions}/>
-      <Alert text="Останнє попередження" type="warning"/>
-      <Alert text="Ура! Все ок!" type="success"/>
-      <Alert text="О жах - все пропало!" type="error"/>
       <GlobalStyle/>
     </div>
   )
---
+++ src/components/Pokemon/PokemonInfo.jsx
@@ -0,0 +1,88 @@
+
+/**
+ * Введіть ім'я покемона
+ * Завантажуємо...
+ * Покемона з _ім'я покемона не знайдено
+ * Покемона {this.props.name} не знайдено
+ * 'idle'
+ * 'pending'
+ * 'resolved'
+ * 'rejected'
+ */
+
+
+
+
+
+// export const PokemonInfo = ({pokemonName}) => {
+
+// const [pokemon, setPokemon] = useState(null);
+// const [error, setError] = useState(null);
+// const [status, setStatus] = useState('idle');
+// console.log(pokemon);
+// useEffect(()=>{
+
+//  if(!pokemonName){
+//   return;
+//  }
+
+//  console.log('Запуск fetch');
+//     setStatus('pending')
+//       fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//   .then(response=>{
+//     if(response.ok === true){
+//        return response.json()
+//     }
+//    return Promise.reject(new Error(`Покемона ${pokemonName} не знайдено`))
+//   })        
+//   .then(pokemon => {
+//     setPokemon(pokemon);
+//     setStatus('resolved');
+//   })
+//   .catch((error)=> {
+//     setError(error);
+//     setStatus('rejected');
+//   })
+//   },[pokemonName]
+// )  
+//       switch (status) {
+//         case 'idle':
+//           return <p>Введіть ім'я покемона</p>;
+
+//         case 'pending':
+//           return <Bars
+//           visible={true}
+//           height="80"
+//           width="80"
+//           ariaLabel="dna-loading"
+//           wrapperStyle={{}}
+//           wrapperClass="dna-wrapper"
+//         />;
+
+//         case 'resolved':
+//           return <div>
+//           <img
+//             src={pokemon.sprites.other['official-artwork'].front_default}
+//             width="240"
+//             height="100"
+//             alt={pokemon.name}
+//           />
+//           <h2>{pokemon.name}</h2>
+//           <ul>
+//             {pokemon.stats.map(entry => (
+//               <li key={entry.stat.name}>
+//                 {entry.stat.name}: {entry.base_stat}
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+          
+//           case 'rejected':
+//           return <p>{error.message}</p>
+             
+//         default:
+//           return;
+//       }
+
+    
+// }
