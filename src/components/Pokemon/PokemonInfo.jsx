7ff91715bd88ff9e70a9188045362d59443a3730
Iryna <irasokolova25@ukr.net>
2024-03-02 13:01:06 +0200

    Update PokemonInfo.jsx

--- src/components/Pokemon/PokemonInfo.jsx
+++ src/components/Pokemon/PokemonInfo.jsx
@@ -15,36 +15,42 @@
 import { PureComponent } from "react";
 
 
-export class PokemonInfo extends PureComponent{
+export class PokemonInfo extends PureComponent {
     state = {
         pokemon: '',
-        isLoading: false
+        error: null,
+        status: 'idle'
     }
 
     componentDidUpdate(prevProps) {
         if (prevProps.pokemonName !== this.props.pokemonName) {
-            this.setState({ isLoading: true, pokemon: null })
+            this.setState({ status: 'pending', pokemon: null })
             
-            setTimeout(() => {
-                fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.pokemonName}`)
-                .then(res => res.json())
-                .then(pokemon =>this.setState({ pokemon, isLoading: false })
-                ) 
-            }, 1000)           
-        }        
+            fetch(`https://pokeapi.co/api/v2/pokemon/${this.props.pokemonName}`)
+                .then(res => {
+                    if (!res.ok) {
+                        return Promise.reject(new Error(`Покемона з іменем ${this.props.pokemonName} не знайдено!`))
+                    }
+                    return res.json()
+                })
+                .then(pokemon => this.setState({ pokemon, status: 'resolved' }))
+                .catch(error => this.setState({ error, status: 'rejected' }))
+        }
     }
 
     render() {
-        const {pokemon, isLoading} = this.state
-        return (
-            <>
-                {isLoading && <div>Loading...</div>}
-            {pokemon && <div>
+        const { pokemon, error, status } = this.state
+
+        if (status === 'idle') { return <p>Ведіть ім'я покемона</p> }
+        if (status === 'pending') { return <div>Loading...</div> }
+        if (status === 'rejected') { return <div>{error.message}</div> }
+        if (status === 'resolved') {
+            return <div>
                 <h2>{pokemon.name}</h2>
-                <img src={pokemon.sprites.other['official-artwork'].front_default} alt=""/>
-                </div>}
-                </>
-        )
+                <img src={pokemon.sprites.other['official-artwork'].front_default} alt="" style={{ width: '150px' }} />
+            </div>
+        }
+               
     }
 }
 
