e20b8feb91a4bdae309d26e6e4ba1164bac281a0
Iryna <irasokolova25@ukr.net>
2024-05-25 10:00:52 +0300

    add plan

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+/**
+ * 1 - бібліотека react-router-dom
+ * 2 - встановлюємо собі на проєкт
+ * 3 - BrowserRouter
+ * 4 - Структура URL-рядка
+ * 5 - <Route path="/about" element={<About />} />
+ * 6 - сторінка помилки навігації <Route path="*" element={<NotFound />} />
+ * 7 - Компоненти <Link> та <NavLink>
+ * 8 - <Route path="/blog/:postId" element={<BlogPost />} />
+ * 9 - Хук useParams
+ * 10 - Вкладені маршрути
+ * 11 - <Outlet>
+ * 
+ */
+
+export function App() {
+  return(
+    <div>
+
+      
+    </div>
+  )
+}
+
+
+
+
+
