9370a10bdd4449122474b553fefec7e1076cb36e
Iryna <irasokolova25@ukr.net>
2024-07-06 13:03:29 +0300

    додає логіку фільтрації

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,12 +1,35 @@
 import { Button } from "components/Button/Button";
 import css from "./StatusFilter.module.css";
+import { getFilters } from "redux/selectors";
+import { statusFilters } from "redux/constants";
+import { setStatusFilter } from "redux/actions";
+import { useDispatch, useSelector } from "react-redux";
 
 export const StatusFilter = () => {
+  const filter = useSelector(getFilters);
+  const dispatch = useDispatch();
+ 
+ const handleFilterChange = filter => dispatch(setStatusFilter(filter));
   return (
-    <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
-    </div>
+   <div>
+     <Button
+       selected={filter === statusFilters.all}
+       onClick={() => handleFilterChange(statusFilters.all)}
+     >
+       All
+     </Button>
+     <Button
+       selected={filter === statusFilters.active}
+       onClick={() => handleFilterChange(statusFilters.active)}
+     >
+       Active
+     </Button>
+     <Button
+       selected={filter === statusFilters.completed}
+       onClick={() => handleFilterChange(statusFilters.completed)}
+     >
+       Completed
+     </Button>
+   </div>
   );
 };
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -10,14 +10,22 @@ export const addTask = (text) => {
 }
 }
 
+export const deleteTask = taskId => {
+ return {
+   type: "tasks/deleteTask",
+   payload: taskId,
+ };
+};
+export const toggleCompleted = taskId => {
+ return {
+   type: "tasks/toggleCompleted",
+   payload: taskId,
+ };
+};
+export const setStatusFilter = value => {
+ return {
+   type: "filters/setStatusFilter",
+   payload: value,
+ };
+};
 
-
-{
-    type:'tasks/deleteTask'
-}
-{
-    type:'tasks/changeStatus'
-}
-{
-    type:'filters/changeFilters'
-}
