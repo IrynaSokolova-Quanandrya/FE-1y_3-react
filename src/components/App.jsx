034247ca6e7efef7ac5942ddbb1a3d8464f13aa0
Iryna <irasokolova25@ukr.net>
2024-07-26 16:10:01 +0300

    add

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import { Layout } from "components/Layout/Layout";
+import { AppBar } from "components/AppBar/AppBar";
+import { TaskForm } from "components/TaskForm/TaskForm";
+import { TaskList } from "components/TaskList/TaskList";
+import { useSelector } from "react-redux";
+
+export const App = () => {
+
+  const tasks = useSelector(state=>state.filters)
+
+  // console.log(tasks);
+  return (
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
+  );
+};
---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { taskReducer } from './taskSlice';
+import { filtersReducer } from './filterSlice';
+
+
+export const store = configureStore({
+  reducer: {
+    tasks: taskReducer,
+    filters: filtersReducer
+  },
+ 
+})
+
+
+
+
+
